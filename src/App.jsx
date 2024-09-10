af5abb1bcb46a76387eec117a81616e35bda07ae
Rosariogorosito <aldanagorosito802@gmail.com>
2024-09-09 22:27:34 -0300

    segunda entrega, funcionalidad de la vitsa de detalle de product, estilos, filtro de prodcuctos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css';
-import { Navbar } from './components/Navbar';
-import { ItemListContainer } from './components/ItemListContainer';
+import React from 'react';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <ItemListContainer welcome="Bienvenidos a Dérive deco"/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route/>
+      <Route/>
+        <Route path="/product/:id" element={<ItemDetailContainer />} />
+        <Route path="/category/:category" element={<ItemListContainer />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,114 @@
-import { CartWidget } from "./CartWidget";
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo-derive-sin-fondo.png";
 import "../styles/navbar.css"
 
-export const Navbar = () => {
+const Navbar = () => {
+    const [menuActive, setMenuActive] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
+
+    const scrollToElement = (id) => {
+        if (location.pathname !== "/") {
+            setTimeout(() => {
+                const element = document.getElementById(id);
+                if (element) {
+                    if (menuActive) setMenuActive(false);
+                    element.scrollIntoView({ behavior: "smooth" });
+                }
+            }, 500);
+            navigate(-1);
+        } else {
+            const element = document.getElementById(id);
+            if (element) {
+                if (menuActive) setMenuActive(false);
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    };
+
+    useEffect(() => {
+        const handleScroll = (event) => {
+            if (menuActive) {
+                event.preventDefault();
+                event.stopPropagation();
+                return false;
+            }
+        };
+
+        if (menuActive) {
+            document.body.classList.add("menu-active");
+            document.addEventListener("touchmove", handleScroll, {
+                passive: false,
+            });
+            document.addEventListener("wheel", handleScroll, { passive: false });
+        } else {
+            document.body.classList.remove("menu-active");
+            document.removeEventListener("touchmove", handleScroll);
+            document.removeEventListener("wheel", handleScroll);
+        }
+
+        return () => {
+            document.removeEventListener("touchmove", handleScroll);
+            document.removeEventListener("wheel", handleScroll);
+        };
+    }, [menuActive]);
 
     return (
-        <nav className="navbar-container">
-            <img src={Logo} alt="Logo-dérive" />
-            <ul className="sections-container">
-                <li>Productos</li>
-                <li>Contacto</li>
-                <li>Envios</li>
-            </ul>
-            <CartWidget />
-        </nav>
-    )
-}
+        <header id="header">
+            <nav id="navbar">
+                <div className="navbar-container">
+                    <div className="logo" onClick={() => scrollToElement("navbar")}>
+                        <img src={Logo} alt="Logo-dérive" />
+                    </div>
+                    <ul className={`navbar-links ${menuActive ? "active" : ""}`}>
+                        {/* <li>
+                            <Link onClick={() => scrollToElement("header")}>Inicio</Link>
+                        </li> */}
+
+                        <li>
+                            <Link to="/category/all">
+                                Todos los productos
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/category/mates">
+                                Mates
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/category/tablas">
+                                Tablas
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/category/sets">
+                                Sets
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link onClick={() => scrollToElement("seccion-contacto")}>
+                                Contacto
+                            </Link>
+                        </li>
+
+                    </ul>
+                    <div className="navbar-toggle" onClick={toggleMenu}>
+                        <span className={`bar ${menuActive ? "active" : ""}`}></span>
+                        <span className={`bar ${menuActive ? "active" : ""}`}></span>
+                        <span className={`bar ${menuActive ? "active" : ""}`}></span>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
